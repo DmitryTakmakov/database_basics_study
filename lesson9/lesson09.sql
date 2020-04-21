3f9dad3d0666d6f0439bd82991fb36a9176716b8
Dmitry Takmakov <dj-dat@yandex.ru>
2020-04-21 18:46:43 +0500

    Дополненный скрипт для девятого урока

--- lesson9/lesson09.sql
+++ lesson9/lesson09.sql
@@ -27,15 +27,43 @@ SELECT * FROM names_cats;
 
 DROP FUNCTION IF EXISTS hello;
 CREATE FUNCTION hello ()
-RETURNS VARCHAR(15) NOT DETERMINISTIC
+RETURNS VARCHAR(15) DETERMINISTIC
 BEGIN
 	CASE
-	  WHEN 6 < DATE_FORMAT(NOW(), '%H') < 12 THEN 'Доброе утро!';
-	  WHEN 12 < DATE_FORMAT(NOW(), '%H') < 18 THEN 'Добрый день!';
-	  WHEN 18 < DATE_FORMAT(NOW(), '%H') <= 23 THEN 'Добрый вечер!';
-	  ELSE 'Доброй ночи!';
+	  WHEN DATE_FORMAT(NOW(), '%H') BETWEEN 6 AND 11 THEN RETURN 'Доброе утро!';
+	  WHEN DATE_FORMAT(NOW(), '%H') BETWEEN 12 AND 17 THEN RETURN 'Добрый день!';
+	  WHEN DATE_FORMAT(NOW(), '%H') BETWEEN 18 AND 23 THEN RETURN 'Добрый вечер!';
+	  ELSE RETURN 'Доброй ночи!';
 	END CASE;
-END
+END -- я "допиливал" эту функцию в терминале, поэтому тут без ;
 
+-- В таблице products есть два текстовых поля: name с названием товара и description с его описанием. Допустимо присутствие обоих полей или одно из них. 
+-- Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема. Используя триггеры, добейтесь того, 
+-- чтобы одно из этих полей или оба поля были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию.
 
+DROP TRIGGER IF EXISTS name_not_null;
+CREATE TRIGGER name_not_null BEFORE UPDATE ON products
+  FOR EACH ROW 
+    BEGIN 
+	    IF NEW.name IS NOT NULL OR OLD.description IS NOT NULL THEN 
+	      SET NEW.name = NEW.name;  -- я не уверен в правильности такой записи, но ведь что-то же должно происходить!
+	    ELSE 
+	      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Both fields can not be NULL. Update cancelled.';
+	    END IF;
+    END
+
+DROP TRIGGER IF EXISTS description_not_null;
+CREATE TRIGGER description_not_null BEFORE UPDATE ON products
+  FOR EACH ROW 
+    BEGIN 
+	    IF NEW.description IS NOT NULL OR OLD.name IS NOT NULL THEN 
+	      SET NEW.description = NEW.description;
+	    ELSE 
+	      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Both fields can not be NULL. Update cancelled.';
+	    END IF;
+    END
+
+-- выражения для проверки
+UPDATE products SET name = NULL WHERE id = 1; -- это выражение отрабатывает без проблем
+UPDATE products SET description = NULL WHERE id = 1; -- а вот тут уже срабатывает триггер
 
